20bf8828927dc18246c63456c996e300578f93d6
luca <gm.lollators@gmail.com>
2018-10-23 19:55:42 -0400

    Linked Manager view to create a guest account and vice-versa

--- src/sample/MangerHomeController.java
+++ src/sample/MangerHomeController.java
@@ -1,10 +1,16 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class MangerHomeController {
 
@@ -24,6 +30,19 @@ public class MangerHomeController {
   
 
   public void buttonNewGuestAccount(ActionEvent actionEvent) {
+    try{
+      FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("createNewGuestAccount.fxml"));
+      Parent root1 = (Parent) fxmlLoader.load();
+
+      Stage myStage = new Stage();
+      myStage.setTitle("Create New Guest Account");
+      myStage.setScene(new Scene(root1));
+      myStage.show();
+      ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+    }
+    catch (Exception e){
+      System.err.println("Can't open new window");
+    }
   }
 
   public void viewFloorChart(ActionEvent actionEvent) {
