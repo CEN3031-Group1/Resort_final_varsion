b2f6451e34a39c679d03b8f6cb963e093d39e5de
Lollators <gm.lollators@gmail.com>
2018-10-24 11:17:57 -0400

    Uploaded files
    Added maintenance request support, linked to guest home and vice versa

--- src/sample/MangerHomeController.java
+++ src/sample/MangerHomeController.java
@@ -1,10 +1,16 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class MangerHomeController {
 
@@ -12,18 +18,31 @@ public class MangerHomeController {
   public Button buttonFloorChart;
   public Button buttonAllRequests;
   public Button buttonEmployees;
-  
+
   //these are request radio buttons - they suppose to disappear when req is done
-  public CheckBox radio1;           
+  public CheckBox radio1;
   public CheckBox radio2;
   public CheckBox radio3;
   public CheckBox radio4;
   //when this button is clicked - radio disappears
   public Button buttonDone;
-  
-  
+
+
 
   public void buttonNewGuestAccount(ActionEvent actionEvent) {
+    try{
+      FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("createNewGuestAccount.fxml"));
+      Parent root1 = (Parent) fxmlLoader.load();
+
+      Stage myStage = new Stage();
+      myStage.setTitle("Create New Guest Account");
+      myStage.setScene(new Scene(root1));
+      myStage.show();
+      ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+    }
+    catch (Exception e){
+      System.err.println("Can't open new window");
+    }
   }
 
   public void viewFloorChart(ActionEvent actionEvent) {
